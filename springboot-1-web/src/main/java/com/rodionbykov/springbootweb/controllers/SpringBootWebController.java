67862fd8c2856cd6e09f5a570d13ac6a8137145d
Rodion Bykov <rodionbykov@gmail.com>
2017-09-22 14:58:30 +0200

    Web Hello World application using Spring Boot

---
+++ springboot-1-web/src/main/java/com/rodionbykov/springbootweb/controllers/SpringBootWebController.java
@@ -0,0 +1,14 @@
+package com.rodionbykov.springbootweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SpringBootWebController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
